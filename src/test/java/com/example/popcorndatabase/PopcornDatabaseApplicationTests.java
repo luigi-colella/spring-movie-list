e1da413cad3a68d67294e1c2b6f7693e7cb18a67
lgcolella <luigicolella.1997@gmail.com>
2019-12-30 02:33:13 +0100

    chore: spring boot project setup

---
+++ src/test/java/com/example/popcorndatabase/PopcornDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popcorndatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
