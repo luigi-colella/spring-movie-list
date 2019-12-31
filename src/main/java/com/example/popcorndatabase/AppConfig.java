9e08ebdafbc4bf290d02d8a2503187851c6959bb
lgcolella <luigicolella.1997@mail.com>
2019-12-31 07:04:07 +0100

    feat: added info endpoint

---
+++ src/main/java/com/example/popcorndatabase/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.popcorndatabase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:git.properties")
+public class AppConfig {
+}
---
+++ src/main/java/com/example/popcorndatabase/controller/InfoController.java
@@ -0,0 +1,28 @@
+package com.example.popcorndatabase.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Controller used to retrieve app's info
+ */
+@RestController
+public class InfoController {
+
+    @GetMapping("/info")
+    public Map<String, String> getInfo(
+        @Value("${git.branch}") String branch,
+        @Value("${git.commit.id}") String commitId,
+        @Value("${git.commit.message.full}") String commitMessage
+    ) {
+        Map<String, String> result = new LinkedHashMap<>();
+        result.put("branch", branch);
+        result.put("commit_id", commitId);
+        result.put("commit_message", commitMessage);
+        return result;
+    }
+}
