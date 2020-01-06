26fe9af1a2e5d338824727f7ce88953a246e1726
lgcolella <luigicolella.1997@gmail.com>
2020-01-06 02:14:43 +0100

    feature: added movie entity

---
+++ src/main/java/com/example/popcorndatabase/movie/Movie.java
@@ -0,0 +1,98 @@
+package com.example.popcorndatabase.movie;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    @Size(max = 45)
+    private String title;
+
+    @Size(min = 4, max = 4)
+    private Integer year;
+
+    @Size(max = 30)
+    private String genre;
+
+    private String plot;
+
+    private String actors;
+
+    private String awards;
+
+    private String posterPath;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+}
---
+++ src/main/java/com/example/popcorndatabase/movie/MovieRepository.java
@@ -0,0 +1,6 @@
+package com.example.popcorndatabase.movie;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+}
