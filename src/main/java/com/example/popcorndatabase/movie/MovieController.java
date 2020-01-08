d55f9bebd22b1afe2ad40121463097945f5fc506
lgcolella <luigicolella.1997@gmail.com>
2020-01-08 22:30:13 +0100

    feature: added comments

--- src/main/java/com/example/popcorndatabase/movie/MovieController.java
+++ src/main/java/com/example/popcorndatabase/movie/MovieController.java
@@ -10,16 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 import java.util.Map;
 
+/**
+ * Controller used to handle CRUD operations and views related to movies.
+ */
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
 
+    /**
+     * View names.
+     */
     public static final String LIST_MOVIES = "movie/listMovies";
     public static final String CREATE_MOVIE_FORM = "movie/createMovieForm";
 
     @Autowired
     MovieRepository movieRepository;
 
+    /**
+     * Show the view with the movies list.
+     */
     @GetMapping("")
     public String showMovies (Map<String, Object> model) {
         Iterable<Movie> movies = movieRepository.findAll();
@@ -27,6 +36,9 @@ public class MovieController {
         return LIST_MOVIES;
     }
 
+    /**
+     * Show the view with the form to add a new movie.
+     */
     @GetMapping("/new")
     public String showMovieForm (Map<String, Object> model) {
         Movie movie = new Movie();
@@ -34,6 +46,9 @@ public class MovieController {
         return CREATE_MOVIE_FORM;
     }
 
+    /**
+     * Save a movie in the database.
+     */
     @PostMapping("")
     public String addMovie (@Valid Movie movie, BindingResult bindingResult, Map<String, Object> model) {
         if (bindingResult.hasErrors()) {
