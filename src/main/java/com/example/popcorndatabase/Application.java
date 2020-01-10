c5e52f04552fa2fe7523377ba718e8d44bed6e05
lgcolella <luigicolella.1997@gmail.com>
2020-01-10 21:48:16 +0100

    refactor: moved app's configuration

--- src/main/java/com/example/popcorndatabase/Application.java
+++ src/main/java/com/example/popcorndatabase/Application.java
@@ -2,12 +2,25 @@ package com.example.popcorndatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@PropertySource("classpath:git.properties")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean hiddenHttpMethodFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HiddenHttpMethodFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegistrationBean;
+    }
 }
