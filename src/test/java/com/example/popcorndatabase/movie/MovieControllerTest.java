45449bea4c0c56e246f6ca09525513525cb67596
lgcolella <luigicolella.1997@gmail.com>
2020-01-12 07:08:18 +0100

    refactor: reformatted code

--- src/test/java/com/example/popcorndatabase/movie/MovieControllerTest.java
+++ src/test/java/com/example/popcorndatabase/movie/MovieControllerTest.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.validation.FieldError;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
