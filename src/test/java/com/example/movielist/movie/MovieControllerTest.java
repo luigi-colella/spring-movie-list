c99e0e8dfb1c69f848130b679bd573465bf9418a
lgcolella <luigicolella.1997@gmail.com>
2020-01-13 01:46:24 +0100

    feature: added database seeder

--- src/test/java/com/example/movielist/movie/MovieControllerTest.java
+++ src/test/java/com/example/movielist/movie/MovieControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import javax.sql.DataSource;
 import java.util.Iterator;
 import java.util.Optional;
 
@@ -24,6 +28,12 @@ public class MovieControllerTest {
 
     @Autowired
     private MovieService movieService;
+
+    @BeforeTestClass
+    @Autowired
+    public void initDatabase(DataSource dataSource) {
+        JdbcTestUtils.deleteFromTables(new JdbcTemplate(dataSource),"movie");
+    }
 
     @Test
     public void bindingShouldSetEmptyStringsToNull() throws Exception {
